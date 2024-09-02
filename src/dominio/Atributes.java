211afde7adce469911f993bd00999106baaffdbb
thednz <augustinpro90@gmail.com>
2024-09-02 06:20:40 -0300

    agora vai

--- src/dominio/Atributes.java
+++ src/dominio/Atributes.java
@@ -1,4 +1,40 @@
 package dominio;
 
 public class Atributes {
+    public String nome;
+    public byte idade;
+    public double salario01;
+    public double salario02;
+    public double salario03;
+    private String nomeDaPessoa;
+
+    public void imprimir() {
+        System.out.println("nome: " + this.nome);
+        System.out.println("idade: " + this.idade);
+        System.out.println("salario 1: " + this.salario01);
+        System.out.println("salario 2: " + this.salario02);
+        System.out.println("salario 3: " + this.salario03);
+
+    }
+
+    public void mediaSalariao(double salario01, double salario02, double salario03) {
+        double total = (salario01 + salario02 + salario03);
+        total /= 3;
+        System.out.println(total);
+    }
+
+    public void setNomeDaPessoa(String x) {
+        nomeDaPessoa = x;
+
+    }
+public String getNomeDaPessoa(){
+    System.out.println(nomeDaPessoa);
+
+    return "";
 }
+
+
+}
+
+
+
--- src/test/Main.java
+++ src/test/Main.java
@@ -1,4 +1,36 @@
 package test;
 
+import dominio.Atributes;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        Atributes things = new Atributes();
+        things.setNomeDaPessoa("Pedrin");
+        things.getNomeDaPessoa();
+        // questionario //
+        System.out.println("Qual o seu nome ?");
+        things.nome = read.nextLine();
+        System.out.println("Qual o sua idade ?");
+        things.idade = read.nextByte();
+        System.out.println("Qual o seu salario01 ?");
+        things.salario01 = read.nextDouble();
+        System.out.println("Qual o seu salario02 ?");
+        things.salario02 = read.nextDouble();
+        System.out.println("Qual o seu salario03 ?");
+        things.salario03 = read.nextDouble();
+        // methods
+        System.out.println("-------------------------------");
+        System.out.println("Esses são seus atribuitos até agora!! ");
+        things.imprimir();
+
+        // method media salarial
+
+        System.out.println("Agora vamos ver a média do seus 3 salários!! ");
+        things.mediaSalariao(things.salario01, things.salario02, things.salario03);
+
+
+    }
 }
